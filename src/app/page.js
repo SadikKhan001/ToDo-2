a0e3eda95df39cff6e63ac0174009b081d950147
SadikKhan001 <sadik.14a@gmail.com>
2025-12-23 23:49:31 +0530

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,117 +1,86 @@
-'use client'
+"use client";
 
-import Image from "next/image";
 import { useEffect, useState } from "react";
 
-import c from 'ansi-colors'
-
 export default function Home() {
+  const statusArray = ["Pending", "Current", "Completed"];
 
-  const statusArray = ['Pending', 'Current', 'Completed'];
+  const [currentStatus, setCurrentStatus] = useState("pending");
+  const [editIndex, setEditIndex] = useState(null);
 
-  const [currentStatus, setCurrentStatus] = useState('Current');
+  const [tasks, setTasks] = useState([]);
 
-  const [tasks, setTasks] = useState([
-    {
-      title: 'sdfsdf',
-      description: 'sflksdfjlkdsf',
-      status: 'sdfdsf'
-    },
-    {
-      title: 'sdfsdf',
-      description: 'sflksdfjlkdsf',
-      status: 'sdfdsf'
-    },
-    {
-      title: 'sdfsdf lkdfj lj jl fjdslk fjds jdsfjf l',
-      description: 'sflksdfjlkdsf',
-      status: 'sdfdsf'
-    },
-    {
-      title: 'sdfsdf',
-      description: 'sflksdfjlkdsf',
-      status: 'sdfdsf'
-    },
-    {
-      title: 'sdfsdf',
-      description: 'sflksdfjlkdsf',
-      status: 'sdfdsf'
-    },
-  ]);
   const [showError, setShowError] = useState(false);
 
-
   const [task, setTask] = useState({
-    title: '',
-    description: '',
-    status: 'Pending'
-  })
+    title: "",
+    description: "",
+    status: "Pending",
+  });
 
-  const [showForm, setShowForm] = useState(false)
-
+  const [showForm, setShowForm] = useState(false);
 
   const handleClose = () => {
-    console.log(c.greenBright('CLosed'))
-    setShowForm(false)
-
-  }
+    setShowForm(false);
+  };
 
   useEffect(() => {
-    console.log(c.redBright(`Updated Tasks: ${JSON.stringify(tasks, null, 2)}`))
-  }, [tasks])
+    const stored = localStorage.getItem("tasks")
+    if(stored){
+      setTasks(JSON.parse(stored));
+    }
+  },[])
 
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const handleAddTasks = () => {
-
     if (task.title.length <= 0 || task.description.length <= 0) {
       setShowError(true);
-      return
+      return;
     }
 
-
-    console.log(c.blueBright('Task Added'))
-
-    console.log(c.blueBright(`${JSON.stringify(task, null, 2)}`))
-
-    setTasks(prev => [...prev, task])
-
+    if (editIndex !== null) {
+      const updatedTasks = tasks.map((item, index) =>
+        index === editIndex ? task : item
+      );
+      setTasks(updatedTasks);
+      localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+      setEditIndex(null);
+    } else {
+      setTasks([...tasks, task]);
+    }
     setTask({
-      title: '',
-      description: '',
-      status: 'Pending'
-    })
+      title: "",
+      description: "",
+      status: "Pending",
+    });
 
-    // currentStatus()
-    setCurrentStatus('Pending')
-    setShowError(false)
-    setShowForm(false)
-
-
-
-  }
+    setCurrentStatus("Pending");
+    setShowError(false);
+    setShowForm(false);
+  };
 
   return (
     <div className="bg-gray-900 min-h-screen w-screen">
-
       <div className="bg-gray-700 px-6 min-w-full flex justify-between items-center py-2">
-
         <p>You Daily To-Do List</p>
 
         <button
           onClick={() => {
-            setShowForm(true)
+            setShowForm(true);
           }}
-          className="text-black text-md bg-purple-400 px-4 py-1 rounded-full">
-          Add new task +
+          className="text-black text-md bg-purple-400 px-4 py-1 rounded-full"
+        >
+          ➕ Add new task
         </button>
       </div>
 
       {/* {tasks.length > 0 && ( */}
       {true && (
         <div className="grid grid-cols-4 flex gap-3 w-full px-4 py-3">
-
           {tasks.map((item, idx) => {
-
             return (
               <div
                 key={idx}
@@ -135,9 +104,10 @@ export default function Home() {
                   {/* Status Chip */}
                   <span
                     className={`px-3 py-1 text-xs font-medium rounded-full w-full text-center
-                      ${item.status === "Completed"
-                        ? "bg-green-500/20 text-green-400 border border-green-500/40"
-                        : item.status === "Current"
+                      ${
+                        item.status === "Completed"
+                          ? "bg-green-500/20 text-green-400 border border-green-500/40"
+                          : item.status === "Current"
                           ? "bg-yellow-500/20 text-yellow-400 border border-yellow-500/40"
                           : "bg-red-500/20 text-red-400 border border-red-500/40"
                       }`}
@@ -152,73 +122,73 @@ export default function Home() {
                 </div>
 
                 <div className="flex gap-2">
-                  <button className="bg-gray-500 rounded-lg px-3 py-1 w-full">Edit</button>
                   <button
-                  onClick={() => {
-
-                   const filteredTasks = tasks.filter((_, index) => index !== idx);
-                   setTasks(filteredTasks)
-
-
-                    
-
-                  }}
-                  
-                  className="bg-red-500 rounded-lg px-3 py-1 w-full">Delete</button>
+                    onClick={() => {
+                      setTask(item);
+                      setCurrentStatus(item.status);
+                      setEditIndex(idx);
+                      setShowForm(true);
+                    }}
+                    className="bg-gray-500 rounded-lg px-3 py-1 w-full"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => {
+                      const filteredTasks = tasks.filter(
+                        (item, index) => index !== idx
+                      );
+                      setTasks(filteredTasks);
+                    }}
+                    className="bg-red-500 rounded-lg px-3 py-1 w-full"
+                  >
+                    Delete
+                  </button>
                 </div>
               </div>
             );
-
-
           })}
-
         </div>
-
-
       )}
-
 
       {showForm && (
         <div className="w-full min-h-screen absolute flex justify-center items-center top-0 left-0">
-
-
           <div className="h-100 w-150 bg-gray-800 relative rounded-3xl flex flex-col justify-center items-center ">
-
             <button
               onClick={() => {
                 handleClose();
               }}
-
-              className="text-black bg-purple-100 px-4 py-1 rounded-full absolute top-5 right-5">X</button>
+              className="text-black bg-purple-100 px-4 py-1 rounded-full absolute top-5 right-5"
+            >
+              X
+            </button>
 
             <div className="w-[60%]">
-
               <div className="">
                 <input
-                  type='text'
+                  type="text"
                   value={task.title}
                   placeholder="Title"
                   onChange={(e) => {
                     const text = e.target.value;
-                    setShowError(false)
+                    setShowError(false);
                     // console.log(`Title text is: ${text}`);
-                    setTask(prev => ({
+                    setTask((prev) => ({
                       ...prev,
                       title: text,
                     }));
                   }}
-
                   className="bg-gray-600 block text-white px-4 py-2 outline-none rounded-full w-full"
                 />
                 <input
-                  type='text'
+                  type="text"
                   value={task.description}
                   placeholder="Description"
                   onChange={(e) => {
                     const text = e.target.value;
-                    setShowError(false)
+                    setShowError(false);
                     // console.log(`Desc text is: ${text}`);
-                    setTask(prev => ({
+                    setTask((prev) => ({
                       ...prev,
                       description: text,
                     }));
@@ -227,7 +197,9 @@ export default function Home() {
                 />
 
                 {showError && (
-                  <p className="mt-2 border border-red-600 rounded-md bg-red-600/20 px-2 text-sm text-center text-red-600">Title and description are required</p>
+                  <p className="mt-2 border border-red-600 rounded-md bg-red-600/20 px-2 text-sm text-center text-red-600">
+                    Title and description are required
+                  </p>
                 )}
               </div>
 
@@ -236,17 +208,21 @@ export default function Home() {
                   return (
                     <button
                       onClick={() => {
-                        setCurrentStatus(item)
-                        setTask(prev => ({
+                        setCurrentStatus(item);
+                        setTask((prev) => ({
                           ...prev,
                           status: item,
-                        }))
+                        }));
                       }}
-
-                      key={idx} className={`border border-green-100 px-4 py-2 rounded-full cursor-pointer ${item === currentStatus && 'bg-green-200/30'}`}>
+                      key={idx}
+                      value={task.status}
+                      className={`border border-green-100 px-4 py-2 rounded-full cursor-pointer ${
+                        item === currentStatus && "bg-green-200/30"
+                      }`}
+                    >
                       <p className="text-green-100 text-sm">{item}</p>
                     </button>
-                  )
+                  );
                 })}
               </div>
 
@@ -254,22 +230,16 @@ export default function Home() {
                 onClick={() => {
                   handleAddTasks();
                 }}
-
-
-                className="bg-purple-400 mt-4 w-full rounded-full px-6 font-semibold text-sm py-2 text-black">Add Task</button>
-
+                className="bg-purple-400 mt-4 w-full rounded-full px-6 font-semibold text-sm py-2 text-black"
+              >
+                {editIndex === null ? "➕add task" : "✏️updated Task"}
+              </button>
             </div>
-
-
           </div>
-
         </div>
-
       )}
-
-
-
-
     </div>
   );
 }
+
+
